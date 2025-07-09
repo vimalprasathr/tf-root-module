c6b99d5b57ddaf28387aa290738573b4c741f99e
Vimal Prasath <44838054+vimalprasathr@users.noreply.github.com>
2025-07-09 18:59:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,3 +47,21 @@ variable "client_secret" {
   description = "Client Secret of the Azure Service Principal"
   sensitive   = true
 }
+# ACR (Azure Container Registry)
+
+variable "acr_name" {
+  description = "The name of the Azure Container Registry"
+  type        = string
+}
+
+variable "acr_sku" {
+  description = "The SKU of the Azure Container Registry (e.g. Basic, Standard, Premium)"
+  type        = string
+  default     = "Basic"
+}
+
+variable "admin_enabled" {
+  description = "Whether the admin user is enabled for the ACR"
+  type        = bool
+  default     = false
+}
