2905e56581d4e07b5d6eddff22a7034ac564a300
Vimal Prasath <44838054+vimalprasathr@users.noreply.github.com>
2025-07-09 18:07:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
+#storage_account
 module "storage_account" {
   source                 = "git::https://github.com/vimalprasathr/tf-module.git//modules?ref=v1.0.0"
   resource_group_name    = var.resource_group_name
